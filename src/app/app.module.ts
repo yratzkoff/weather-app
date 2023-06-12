a69d4fe51b4ac813872d77d993c7fe3c5fd09619
yratzkoff <yratzkoff@gmail.com>
2023-06-12 11:44:03 +0300

    generate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BackgroundComponent } from './background/background.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BackgroundComponent
+    BackgroundComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
