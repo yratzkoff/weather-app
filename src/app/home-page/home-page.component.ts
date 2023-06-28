e8bd3f908161524f605ea1bd64a76b2a013286a8
yratzkoff <yratzkoff@gmail.com>
2023-06-28 22:03:52 +0300

    add(location name)

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
+
+  @Input() locationName: string = "ןדרשקך";
 
   constructor() { }
 
